85c914c21ece616df71d67ae041682776e7805a3
Boris Shvidchenko <101944376+boris-shvidchenko@users.noreply.github.com>
2022-07-19 20:05:06 -0700

    Update data.js

--- travel-journal/data.js
+++ travel-journal/data.js
@@ -1,5 +1,6 @@
 export default [
     {
+        key: 1,
         title: "Canyonlands",
         location: "Utah, USA",
         googleMapsUrl: "https://www.google.com/maps/place/Canyonlands+National+Park/@38.2643128,-109.9250343,11.25z/data=!4m12!1m6!3m5!1s0x8747e1ee4518a6a9:0x15a452a9c502e6aa!2sCanyonlands+National+Park!8m2!3d38.2135733!4d-109.9025345!3m4!1s0x8747e1ee4518a6a9:0x15a452a9c502e6aa!8m2!3d38.2135733!4d-109.9025345",
@@ -9,6 +10,7 @@ export default [
         imageUrl: "./images/Utah.jpg"
     },
     {
+        key: 2,
         title: "Roldanillo",
         location: "Valle del Cauca, Colombia",
         googleMapsUrl: "https://www.google.com/maps/place/Roldanillo,+Valle+del+Cauca,+Colombia/@4.4141378,-76.1609278,15z/data=!3m1!4b1!4m5!3m4!1s0x8e383714cff81901:0xdbf713a1f87e02ee!8m2!3d4.4154736!4d-76.1513942",
@@ -18,6 +20,7 @@ export default [
         imageUrl: "./images/Colombia.jpg"
     },
     {
+        key: 3,
         title: "Interlaken",
         location: "Switzerland",
         googleMapsUrl: "https://www.google.com/maps/place/Interlaken,+Switzerland/@46.6844986,7.8403204,14z/data=!3m1!4b1!4m5!3m4!1s0x1453996a31921a05:0x9c111af14c02be00!8m2!3d46.6863481!4d7.8632049",
