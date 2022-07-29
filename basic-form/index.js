62d17ddc8475de656e5a21c46a01b2f3b0865841
Boris Shvidchenko <101944376+boris-shvidchenko@users.noreply.github.com>
2022-07-29 09:18:35 -0700

    Update index.js

--- basic-form/index.js
+++ basic-form/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+// Here, the final step of rendering the page is performed. App is imported, and rendered to the DOM.
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
