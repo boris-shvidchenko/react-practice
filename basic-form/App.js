3c73d3795ca18a99cb78b8ff95c35b1c89038308
Boris Shvidchenko <101944376+boris-shvidchenko@users.noreply.github.com>
2022-07-29 09:17:23 -0700

    Update App.js

--- basic-form/App.js
+++ basic-form/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Form from "./Form.js";
 
+// App collects all components into one location, before returning the "final" page/app. In this case, only one component is imported (Form).
 function App() {
   return (
     <div className="App">
